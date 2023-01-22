66e9aed606d9cfb5c999401b140f1bcc8c74dd69
Jakub Radzik <jradzik4@gmail.com>
2023-01-22 12:15:55 +0100

    cors and axios

--- backend/api.py
+++ backend/api.py
@@ -1,18 +1,18 @@
-from flask import Flask, request, jsonify, make_response
-from flask_mongoengine import MongoEngine
 import datetime
-from flask_cors import CORS
-from flask_pymongo import PyMongo
 
+from flask import Flask, request, jsonify, make_response
+from flask_cors import CORS
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 
 DB_URI = "mongodb+srv://admin:admin@m001.hd4gnzi.mongodb.net/iot?retryWrites=true&w=majority"
 app.config["MONGODB_HOST"] = DB_URI
+CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 db = MongoEngine(app)
 
-CORS(app)
 
 # klasa do odzwierciedlenia obiektow w bazie
 
@@ -40,11 +40,9 @@ def addLog():
     reader = request.json.get("reader", None)
 
     date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%fZ')
-    print("chuj")
 
     new_log = logs(log_id=log_id, date=date, card_uid=card_uid, reader=reader)
     new_log.save()
-    print("inny chuj")
 
     return make_response("", 201)
 
@@ -53,7 +51,6 @@ def addLog():
 def getLogs():
     all_logs = [log for log in logs.objects]
     return make_response(jsonify(all_logs), 200)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
