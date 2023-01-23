512f16f44a9a96563e4f94c4e3d6e12916324727
Jakub Radzik <jradzik4@gmail.com>
2023-01-23 13:41:21 +0100

    websockets

--- frontend/src/hooks/useLogs.tsx
+++ frontend/src/hooks/useLogs.tsx
@@ -1,8 +1,17 @@
 import { Log } from "../types";
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import { useEffect, useState } from "react";
+import useWebSocket from 'react-use-websocket';
+
+const socketUrl = 'ws://localhost:7001';
 
 export const useLogs = () => {
+  const {sendMessage} = useWebSocket(socketUrl, {
+    onOpen: () => console.log('opened'),
+    onMessage: (message) => console.log(message),
+    onClose: () => console.log('closed'),
+    onError: (error) => console.log(error),
+  });
 
   const [logs, setLogs] = useState<Log[]>([]);
 
