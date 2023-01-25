6950c5d732f72073766087f240b4fbdef98349eb
piotrwojdan <piotr.wojdan2001@gmail.com>
2023-01-25 18:32:21 +0100

    zmieniony back na zajeciach

--- backend/api.py
+++ backend/api.py
@@ -1,18 +1,18 @@
-from flask import Flask, request, jsonify, make_response
-from flask_mongoengine import MongoEngine
 import datetime
+import json
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
-from flask_pymongo import PyMongo
-
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 
 DB_URI = "mongodb+srv://admin:admin@m001.hd4gnzi.mongodb.net/iot?retryWrites=true&w=majority"
 app.config["MONGODB_HOST"] = DB_URI
+CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 db = MongoEngine(app)
 
-CORS(app)
 
 # klasa do odzwierciedlenia obiektow w bazie
 
@@ -34,26 +34,32 @@ class logs(db.Document):
 
 @app.route("/logs/add", methods=['POST'])
 def addLog():
-    log_id = request.json.get("id", None)
-    date = request.json.get("date", None)
-    card_uid = request.json.get("card_uid", None)
-    reader = request.json.get("reader", None)
 
-    date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%fZ')
+    json_from_request = json.loads(request.json)
 
+    if (type(request.json) is str):
+        log_id = json_from_request.get("id", None)
+        date = json_from_request.get("date", None)
+        card_uid = json_from_request.get("card_uid", None)
+        reader = json_from_request.get("reader", None)
+    else:
+        log_id = request.json.get("id", None)
+        date = request.json.get("date", None)
+        card_uid = request.json.get("card_uid", None)
+        reader = request.json.get("reader", None)
+
+    date = datetime.datetime.strptime(date, '%d-%m-%Y %H:%M:%S')
 
     new_log = logs(log_id=log_id, date=date, card_uid=card_uid, reader=reader)
     new_log.save()
-
 
     return make_response("", 201)
 
 
 @app.route("/logs/get", methods=['GET'])
 def getLogs():
-    all_logs = [log for log in logs.objects]
+    all_logs = [log.to_json() for log in logs.objects]
     return make_response(jsonify(all_logs), 200)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
